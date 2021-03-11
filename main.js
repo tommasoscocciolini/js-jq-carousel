9783cf1406bb521e5b1433cd56d8d3856ced1253
tommaso <scocciolinitommaso@gmail.com>
2021-03-11 16:20:22 +0100

    definizione delle funzioni, alleggerimento codice

--- main.js
+++ main.js
@@ -1,78 +1,58 @@
 $(document).ready(function(){
   var iconNext = $(".fa-angle-right");
   var iconPrev = $(".fa-angle-left");
-  // var imagesBox = $(".images")
+
 
   iconNext.click(function(){
-    var imageEl = $("img.active");
-    var dotEl = $("i.active")
-    if (imageEl.hasClass("last")) {
-      $(".images").children(":first").addClass("active");
-      imageEl.removeClass("active");
-
-      $(".nav").children(":first").addClass("active");
-      dotEl.removeClass("active");
-    } else {
-      imageEl.next().addClass("active");
-      imageEl.removeClass("active");
-
-      dotEl.next().addClass("active");
-      dotEl.removeClass("active");
-    }
-  })
+    nextImg();
+    })
 
   iconPrev.click(function(){
-    var imageEl = $("img.active");
-    var dotEl = $("i.active");
-    if (imageEl.hasClass("first")) {
-      $(".images").children(":last").addClass("active");
-      imageEl.removeClass("active");
-
-      $(".nav").children(":last").addClass("active");
-      dotEl.removeClass("active");
-    } else {
-      imageEl.prev().addClass("active");
-      imageEl.removeClass("active");
-
-      dotEl.prev().addClass("active");
-      dotEl.removeClass("active");
-    }
+    prevImg()
   })
-
 })
 
 $(document).keydown(function(e){
   if (e.keyCode==37) {
-    var imageEl = $("img.active");
-    var dotEl = $("i.active");
-    if (imageEl.hasClass("first")) {
-      $(".images").children(":last").addClass("active");
-      imageEl.removeClass("active");
-
-      $(".nav").children(":last").addClass("active");
-      dotEl.removeClass("active");
-    } else {
-      imageEl.prev().addClass("active");
-      imageEl.removeClass("active");
-
-      dotEl.prev().addClass("active");
-      dotEl.removeClass("active");
-    }
+    prevImg();
   } else if (e.keyCode==39) {
-    var imageEl = $("img.active");
-    var dotEl = $("i.active")
-    if (imageEl.hasClass("last")) {
-      $(".images").children(":first").addClass("active");
-      imageEl.removeClass("active");
-
-      $(".nav").children(":first").addClass("active");
-      dotEl.removeClass("active");
-    } else {
-      imageEl.next().addClass("active");
-      imageEl.removeClass("active");
-
-      dotEl.next().addClass("active");
-      dotEl.removeClass("active");
-    }
+    nextImg();
   }
 })
+
+
+function nextImg(){
+  var imageEl = $("img.active");
+  var dotEl = $("i.active")
+  if (imageEl.hasClass("last")) {
+    $(".images").children(":first").addClass("active");
+    imageEl.removeClass("active");
+
+    $(".nav").children(":first").addClass("active");
+    dotEl.removeClass("active");
+  } else {
+    imageEl.next().addClass("active");
+    imageEl.removeClass("active");
+
+    dotEl.next().addClass("active");
+    dotEl.removeClass("active");
+}
+}
+
+function prevImg(){
+  var imageEl = $("img.active");
+  var dotEl = $("i.active");
+  if (imageEl.hasClass("first")) {
+    $(".images").children(":last").addClass("active");
+    imageEl.removeClass("active");
+
+    $(".nav").children(":last").addClass("active");
+    dotEl.removeClass("active");
+  } else {
+    imageEl.prev().addClass("active");
+    imageEl.removeClass("active");
+
+    dotEl.prev().addClass("active");
+    dotEl.removeClass("active");
+  }
+}
